b2bc647d3d73f136f38dbe5a3a0ce49e5fdf0fa6
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-07 19:48:41 -0400

    Added butterkine code and views references

--- app/src/main/java/com/example/felipe/androidchat/LoginActivity.java
+++ app/src/main/java/com/example/felipe/androidchat/LoginActivity.java
@@ -1,13 +1,52 @@
 package com.example.felipe.androidchat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.txtEmail)
+    EditText txtEmail;
+    @Bind(R.id.txtPassword)
+    EditText txtPassword;
+    @Bind(R.id.wrapperPassword)
+    TextInputLayout wrapperPassword;
+    @Bind(R.id.btnSignIn)
+    Button btnSignIn;
+    @Bind(R.id.btnSignUp)
+    Button btnSignUp;
+    @Bind(R.id.layoutButtons)
+    LinearLayout layoutButtons;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+    @Bind(R.id.layoutMainContainer)
+    RelativeLayout layoutMainContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btnSignIn, R.id.btnSignUp})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSignIn:
+                break;
+            case R.id.btnSignUp:
+                break;
+        }
     }
 }
