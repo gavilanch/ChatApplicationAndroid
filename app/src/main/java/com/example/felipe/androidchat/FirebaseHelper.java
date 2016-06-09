2b8084cb70cdd263a7a0be927105f280088e47b5
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-08 20:18:22 -0400

    Added FirebaseHelper class

---
+++ app/src/main/java/com/example/felipe/androidchat/FirebaseHelper.java
@@ -0,0 +1,43 @@
+package com.example.felipe.androidchat;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+
+import java.util.Map;
+
+public class FirebaseHelper {
+    private Firebase dataReference;
+    private final static String SEPARATOR = "__";
+    private final static String USERS_PATH = "users";
+    private final static String CHATS_PATH = "chats";
+    private final static String CONTACTS_PATH = "contacts";
+    private final static String FIREBASE_URL = "https://androidchat-d0c33.firebaseio.com/";
+
+    private static class SingletonHolder{
+        private static final FirebaseHelper INSTANCE = new FirebaseHelper();
+    }
+
+    public static FirebaseHelper getInstance(){
+        return SingletonHolder.INSTANCE;
+    }
+
+    public FirebaseHelper(){
+        this.dataReference = new Firebase(FIREBASE_URL);
+    }
+
+    public Firebase getDataReference(){
+        return dataReference;
+    }
+
+    public String getAuthUserEmail(){
+        AuthData authData = dataReference.getAuth();
+        String email = null;
+        if (authData != null){
+            Map<String, Object> providerData = authData.getProviderData();
+            email = providerData.get("email").toString();
+        }
+
+        return email;
+    }
+
+}
