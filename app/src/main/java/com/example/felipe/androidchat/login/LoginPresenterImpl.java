6607233d4237c864ed98d60932cf07306f02957c
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-10 00:52:17 -0400

    Created class LoginPresenterImpl

---
+++ app/src/main/java/com/example/felipe/androidchat/login/LoginPresenterImpl.java
@@ -0,0 +1,26 @@
+package com.example.felipe.androidchat.login;
+
+/**
+ * Created by Felipe on 10-Jun-16.
+ */
+public class LoginPresenterImpl implements LoginPresenter {
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public void checkForAuthenticatedUser() {
+
+    }
+
+    @Override
+    public void validateLogin(String email, String password) {
+
+    }
+
+    @Override
+    public void registerNewUser(String email, String password) {
+
+    }
+}
