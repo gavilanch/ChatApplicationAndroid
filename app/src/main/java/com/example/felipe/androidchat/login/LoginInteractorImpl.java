3250849e9c160df2dd44d530318fae4878b15438
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-10 01:06:07 -0400

    Implemented LoginInteractorImpl methods

--- app/src/main/java/com/example/felipe/androidchat/login/LoginInteractorImpl.java
+++ app/src/main/java/com/example/felipe/androidchat/login/LoginInteractorImpl.java
@@ -1,18 +1,21 @@
 package com.example.felipe.androidchat.login;
 
 public class LoginInteractorImpl implements LoginInteractor {
+
+    private LoginRepository loginRepository;
+
     @Override
     public void checkSession() {
-
+        loginRepository.checkSession();
     }
 
     @Override
     public void doSignUp(String email, String password) {
-
+        loginRepository.signUp(email, password);
     }
 
     @Override
     public void doSignIn(String email, String password) {
-
+        loginRepository.signIn(email, password);
     }
 }
