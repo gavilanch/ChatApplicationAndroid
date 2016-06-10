b350d575906814804a60c10d1a3c27faa462cfb8
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-10 01:41:09 -0400

    Created and implemented GreenRobotEventBus class

---
+++ app/src/main/java/com/example/felipe/androidchat/lib/GreenRobotEventBus.java
@@ -0,0 +1,33 @@
+package com.example.felipe.androidchat.lib;
+
+public class GreenRobotEventBus implements EventBus {
+
+    de.greenrobot.event.EventBus eventBus;
+
+    private static class SingletonHolder{
+        private static final GreenRobotEventBus INSTANCE = new GreenRobotEventBus();
+    }
+
+    public  static GreenRobotEventBus getInstance(){
+        return SingletonHolder.INSTANCE;
+    }
+
+    public GreenRobotEventBus(){
+        this.eventBus = de.greenrobot.event.EventBus.getDefault();
+    }
+
+    @Override
+    public void register(Object suscriber) {
+        eventBus.register(suscriber);
+    }
+
+    @Override
+    public void unregister(Object suscriber) {
+        eventBus.unregister(suscriber);
+    }
+
+    @Override
+    public void post(Object event) {
+        eventBus.post(event);
+    }
+}
