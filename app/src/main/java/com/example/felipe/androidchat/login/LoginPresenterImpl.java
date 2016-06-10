37aaf535e5ae1a6bc3b641a91d2500aa1a84f947
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-10 01:02:49 -0400

    Implemented methods of LoginPresenterImpl

--- app/src/main/java/com/example/felipe/androidchat/login/LoginPresenterImpl.java
+++ app/src/main/java/com/example/felipe/androidchat/login/LoginPresenterImpl.java
@@ -1,26 +1,69 @@
 package com.example.felipe.androidchat.login;
 
-/**
- * Created by Felipe on 10-Jun-16.
- */
 public class LoginPresenterImpl implements LoginPresenter {
+
+    LoginView loginView;
+    LoginInteractor loginInteractor;
+
+    public LoginPresenterImpl(LoginView loginView) {
+        this.loginView = loginView;
+    }
+
     @Override
     public void onDestroy() {
-
+        loginView = null;
     }
 
     @Override
     public void checkForAuthenticatedUser() {
-
+        setViewInWaitMode();
+        loginInteractor.checkSession();
     }
 
     @Override
     public void validateLogin(String email, String password) {
-
+        setViewInWaitMode();
+        loginInteractor.doSignIn(email, password);
     }
 
     @Override
     public void registerNewUser(String email, String password) {
+        setViewInWaitMode();
+        loginInteractor.doSignUp(email, password);
+    }
 
+    private void setViewInWaitMode(){
+        if (loginView != null){
+            loginView.disableInputs();
+            loginView.showProgress();
+        }
+    }
+
+    private void onSignInSuccess(){
+        if (loginView != null){
+            loginView.navigateToMainScreen();
+        }
+    }
+
+    private void onSignUpSuccess(){
+        if (loginView != null){
+            loginView.newUserSuccess();
+        }
+    }
+
+    private void onSignInError(String error){
+        if (loginView != null){
+            loginView.hideProgress();
+            loginView.enableInputs();
+            loginView.loginError(error);
+        }
+    }
+
+    private void onSignUpError(String error){
+        if (loginView != null){
+            loginView.hideProgress();
+            loginView.enableInputs();
+            loginView.newUserError(error);
+        }
     }
 }
