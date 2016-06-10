12123094688bffb384c1ce021062c796c3bce52c
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-10 01:04:15 -0400

    Created class LoginInteractorImpl

---
+++ app/src/main/java/com/example/felipe/androidchat/login/LoginInteractorImpl.java
@@ -0,0 +1,18 @@
+package com.example.felipe.androidchat.login;
+
+public class LoginInteractorImpl implements LoginInteractor {
+    @Override
+    public void checkSession() {
+
+    }
+
+    @Override
+    public void doSignUp(String email, String password) {
+
+    }
+
+    @Override
+    public void doSignIn(String email, String password) {
+
+    }
+}
