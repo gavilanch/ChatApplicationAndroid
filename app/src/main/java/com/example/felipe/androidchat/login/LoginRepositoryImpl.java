2a92bdb1b2d312c4afc20c6d735b66a6cfeb92e7
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-10 01:10:44 -0400

    Created LoginRepositoryImpl class

---
+++ app/src/main/java/com/example/felipe/androidchat/login/LoginRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.example.felipe.androidchat.login;
+
+public class LoginRepositoryImpl implements LoginRepository {
+    @Override
+    public void signUp(String email, String password) {
+
+    }
+
+    @Override
+    public void signIn(String email, String password) {
+
+    }
+
+    @Override
+    public void checkSession() {
+
+    }
+}
