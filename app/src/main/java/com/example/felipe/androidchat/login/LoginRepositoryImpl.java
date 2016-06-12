9844629108c3f61d9d3b023b51084baa9f8cda31
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-11 22:33:30 -0400

    Implementation of eventBus logic

--- app/src/main/java/com/example/felipe/androidchat/login/LoginRepositoryImpl.java
+++ app/src/main/java/com/example/felipe/androidchat/login/LoginRepositoryImpl.java
@@ -3,6 +3,9 @@ package com.example.felipe.androidchat.login;
 import android.util.Log;
 
 import com.example.felipe.androidchat.domain.FirebaseHelper;
+import com.example.felipe.androidchat.lib.EventBus;
+import com.example.felipe.androidchat.lib.GreenRobotEventBus;
+import com.example.felipe.androidchat.login.events.LoginEvent;
 
 public class LoginRepositoryImpl implements LoginRepository {
 
@@ -14,16 +17,32 @@ public class LoginRepositoryImpl implements LoginRepository {
 
     @Override
     public void signUp(String email, String password) {
-        Log.e("LoginRepositoryImpl", "signup");
+        postEvent(LoginEvent.onSignUpSuccess);
     }
 
     @Override
     public void signIn(String email, String password) {
-        Log.e("LoginRepositoryImpl", "signin");
+        postEvent(LoginEvent.onSignInSuccess);
     }
 
     @Override
     public void checkSession() {
-        Log.e("LoginRepositoryImpl", "checkSession");
+      postEvent(LoginEvent.onFailedToRecoverSession);
+    }
+
+    private void postEvent(int type, String errorMessage){
+        LoginEvent loginEvent = new LoginEvent();
+        loginEvent.setEventType(type);
+        if (errorMessage != null){
+            loginEvent.setErrorMessage(errorMessage);
+        }
+
+        EventBus eventBus = GreenRobotEventBus.getInstance();
+        eventBus.post(loginEvent);
+
+    }
+
+    private void postEvent(int type){
+        postEvent(type, null);
     }
 }
