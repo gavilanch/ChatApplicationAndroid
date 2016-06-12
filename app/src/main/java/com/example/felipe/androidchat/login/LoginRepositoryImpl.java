a6dff51c7d92cc4533ec4d6b1c5d486cca3937b5
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-12 15:33:20 -0400

    Implemented signIn logic with firebase

--- app/src/main/java/com/example/felipe/androidchat/login/LoginRepositoryImpl.java
+++ app/src/main/java/com/example/felipe/androidchat/login/LoginRepositoryImpl.java
@@ -3,26 +3,70 @@ package com.example.felipe.androidchat.login;
 import android.util.Log;
 
 import com.example.felipe.androidchat.domain.FirebaseHelper;
+import com.example.felipe.androidchat.entities.User;
 import com.example.felipe.androidchat.lib.EventBus;
 import com.example.felipe.androidchat.lib.GreenRobotEventBus;
 import com.example.felipe.androidchat.login.events.LoginEvent;
+import com.firebase.client.AuthData;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class LoginRepositoryImpl implements LoginRepository {
 
     private FirebaseHelper helper;
+    private Firebase dataReference;
+    private Firebase myUserReference;
 
     public LoginRepositoryImpl() {
         this.helper = FirebaseHelper.getInstance();
+        this.dataReference = helper.getDataReference();
+        this.myUserReference = helper.getMyUserReference();
     }
 
     @Override
     public void signUp(String email, String password) {
         postEvent(LoginEvent.onSignUpSuccess);
+
     }
 
     @Override
     public void signIn(String email, String password) {
-        postEvent(LoginEvent.onSignInSuccess);
+        dataReference.authWithPassword(email, password, new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                myUserReference = helper.getMyUserReference();
+
+
+                myUserReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        User currentUser = dataSnapshot.getValue(User.class);
+
+                        if (currentUser != null){
+                            String email = helper.getAuthUserEmail();
+                            if (email != null){
+                                currentUser = new User();
+                                myUserReference.setValue(currentUser);
+                            }
+                        }
+                        helper.changeUserConnectionStatus(currentUser.ONLINE);
+                        postEvent(LoginEvent.onSignInSuccess);
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {}
+                });
+
+            }
+
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                postEvent(LoginEvent.onSignInError, firebaseError.getMessage());
+            }
+        });
+
     }
 
     @Override
