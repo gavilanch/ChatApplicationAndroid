c1fe1bf6fc1b88bb05c3ebc36d0fc216e6d50c02
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-14 20:40:09 -0400

    added interface ContactListView

---
+++ app/src/main/java/com/example/felipe/androidchat/contactlist/ContactListView.java
@@ -0,0 +1,9 @@
+package com.example.felipe.androidchat.contactlist;
+
+import com.example.felipe.androidchat.entities.User;
+
+public interface ContactListView {
+    void onContactAdded(User user);
+    void onContactChanged(User user);
+    void onContactRemoved(User user);
+}
