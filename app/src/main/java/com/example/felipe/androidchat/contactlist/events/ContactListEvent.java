d9c3ba48867fca3bf740fb5a78515cf89d45b972
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-19 19:54:54 -0400

    Implemented ContactListEvent

--- app/src/main/java/com/example/felipe/androidchat/contactlist/events/ContactListEvent.java
+++ app/src/main/java/com/example/felipe/androidchat/contactlist/events/ContactListEvent.java
@@ -1,4 +1,29 @@
 package com.example.felipe.androidchat.contactlist.events;
 
+import com.example.felipe.androidchat.entities.User;
+
 public class ContactListEvent {
+
+    public final static int onContactAdded = 0;
+    public final static int onContactChanged = 1;
+    public final static int onContactRemoved = 2;
+
+    private User user;
+    private int eventType;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(int eventType) {
+        this.eventType = eventType;
+    }
 }
