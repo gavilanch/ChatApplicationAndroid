fd3d484d9cfc150f8775c7a3f0e07ddde6a08648
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-19 19:07:08 -0400

    Implemented adapter

--- app/src/main/java/com/example/felipe/androidchat/contactlist/ui/adapters/ContactListAdapter.java
+++ app/src/main/java/com/example/felipe/androidchat/contactlist/ui/adapters/ContactListAdapter.java
@@ -1,43 +1,92 @@
 package com.example.felipe.androidchat.contactlist.ui.adapters;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.felipe.androidchat.R;
 import com.example.felipe.androidchat.entities.User;
 
 import java.util.List;
 
-public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.ViewHolder>{
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.ViewHolder> {
+
 
     private List<User> contactList;
-    private ImageLoading imageLoading;
+    private ImageLoading imageLoader;
     private OnItemClickListener onItemClickListener;
 
-    public ContactListAdapter(List<User> contactList, ImageLoading imageLoading, OnItemClickListener onItemClickListener) {
+    public ContactListAdapter(List<User> contactList, ImageLoading imageLoader, OnItemClickListener onItemClickListener) {
         this.contactList = contactList;
-        this.imageLoading = imageLoading;
+        this.imageLoader = imageLoader;
         this.onItemClickListener = onItemClickListener;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.content_contact, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        User user = contactList.get(position);
+        holder.setOnClickListener(user, onItemClickListener);
 
+        String email = user.getEmail();
+        boolean isOnline = user.isOnline();
+        String status = isOnline ? "online" : "offline";
+        int color = isOnline ? Color.GREEN : Color.RED;
+
+        holder.txtUser.setText(email);
+        holder.txtUser.setText(status);
+        holder.txtStatus.setTextColor(color);
+
+        imageLoader.load(holder.imgAvatar, "");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return contactList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.imgAvatar)
+        CircleImageView imgAvatar;
+        @Bind(R.id.txtUser)
+        TextView txtUser;
+        @Bind(R.id.txtStatus)
+        TextView txtStatus;
+        private View view;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+            this.view = itemView;
+        }
+
+        private void setOnClickListener(final User user, final OnItemClickListener listener){
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(user);
+                }
+            });
+
+            view.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    listener.onItemLongClick(user);
+                    return false;
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/felipe/androidchat/contactlist/ui/adapters/ImageLoading.java
+++ app/src/main/java/com/example/felipe/androidchat/contactlist/ui/adapters/ImageLoading.java
@@ -1,4 +1,9 @@
 package com.example.felipe.androidchat.contactlist.ui.adapters;
 
+import android.widget.ImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public interface ImageLoading {
+    void load(ImageView imgAvatar, String url);
 }
