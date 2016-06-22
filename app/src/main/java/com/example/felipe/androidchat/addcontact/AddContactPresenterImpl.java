b59daf8e2a6753692cbd7c311171ed7cb216f29e
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-21 22:16:27 -0400

    Implemented presenter and repository

--- app/src/main/java/com/example/felipe/androidchat/addcontact/AddContactPresenterImpl.java
+++ app/src/main/java/com/example/felipe/androidchat/addcontact/AddContactPresenterImpl.java
@@ -3,32 +3,56 @@ package com.example.felipe.androidchat.addcontact;
 import com.example.felipe.androidchat.addcontact.events.AddContactEvent;
 import com.example.felipe.androidchat.addcontact.ui.AddContactFragment;
 import com.example.felipe.androidchat.addcontact.ui.AddContactView;
+import com.example.felipe.androidchat.lib.EventBus;
+import com.example.felipe.androidchat.lib.GreenRobotEventBus;
+
+import org.greenrobot.eventbus.Subscribe;
 
 public class AddContactPresenterImpl implements AddContactPresenter {
 
-    AddContactView view;
+    private EventBus eventBus;
+    private AddContactInteractor interactor;
+    private AddContactView view;
 
     public AddContactPresenterImpl(AddContactView addContactView) {
-        this.view = view;
+        this.view = addContactView;
+        this.eventBus = GreenRobotEventBus.getInstance();
+        this.interactor = new AddContactInteractorImpl();
     }
 
     @Override
     public void onShow() {
-
+    eventBus.register(this);
     }
 
     @Override
     public void onDestroy() {
-
+        this.view = null;
+        eventBus.unregister(this);
     }
 
     @Override
     public void addContact(String email) {
-
+        if (view != null){
+            view.hideInput();
+            view.showProgress();
+        }
+        interactor.addContact(email);
     }
 
     @Override
+    @Subscribe
     public void onEventMainThread(AddContactEvent event) {
+        if (view != null){
+            view.showInput();
+            view.hideProgress();
+        }
+
+        if (event.isError()){
+            view.contactNotAdded();
+        } else{
+            view.contactAdded();
+        }
 
     }
 }
--- app/src/main/java/com/example/felipe/androidchat/addcontact/AddContactRepositoryImpl.java
+++ app/src/main/java/com/example/felipe/androidchat/addcontact/AddContactRepositoryImpl.java
@@ -1,8 +1,62 @@
 package com.example.felipe.androidchat.addcontact;
 
-public class AddContactRepositoryImpl implements AddContactRepository {
-    @Override
-    public void addContact(String email) {
+import com.example.felipe.androidchat.addcontact.events.AddContactEvent;
+import com.example.felipe.androidchat.domain.FirebaseHelper;
+import com.example.felipe.androidchat.entities.User;
+import com.example.felipe.androidchat.lib.EventBus;
+import com.example.felipe.androidchat.lib.GreenRobotEventBus;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
+public class AddContactRepositoryImpl implements AddContactRepository {
+
+    private EventBus eventBus;
+    private FirebaseHelper helder;
+
+    public AddContactRepositoryImpl() {
+        this.eventBus = GreenRobotEventBus.getInstance();
+        this.helder = FirebaseHelper.getInstance();
+    }
+
+    @Override
+    public void addContact(final String email) {
+        final String key = email.replace(".", "_");
+        Firebase userReference = helder.getUserReference(email);
+        userReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+
+                if (user != null){
+                    Firebase myContactsReference = helder.getMyContactsReference();
+                    myContactsReference.child(key).setValue(user.isOnline());
+
+                    String currentUserKey = helder.getAuthUserEmail();
+                    currentUserKey = currentUserKey.replace(".", "_");
+
+                    Firebase reverseContactReference = helder.getContactsReference(email);
+                    reverseContactReference.child(currentUserKey).setValue(User.ONLINE);
+
+                    post(false);
+
+                }
+                else{
+                    post(true);
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                post(true);
+            }
+        });
+    }
+
+    private void post(boolean error) {
+        AddContactEvent event = new AddContactEvent();
+        event.setError(true);
+        eventBus.post(event);
     }
 }
