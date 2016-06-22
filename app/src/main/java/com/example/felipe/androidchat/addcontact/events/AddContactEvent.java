8d0bdbf837dde77124b2736e8c4efc473c3b9c16
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-21 21:19:58 -0400

    Added error property

--- app/src/main/java/com/example/felipe/androidchat/addcontact/events/AddContactEvent.java
+++ app/src/main/java/com/example/felipe/androidchat/addcontact/events/AddContactEvent.java
@@ -1,4 +1,13 @@
 package com.example.felipe.androidchat.addcontact.events;
 
 public class AddContactEvent {
+    private boolean error = false;
+
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
 }
