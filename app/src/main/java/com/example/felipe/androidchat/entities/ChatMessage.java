a3b82a488383092bd062594a3b49b262551ab0a0
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-27 18:55:59 -0400

    Implemented chatMessage

--- app/src/main/java/com/example/felipe/androidchat/entities/ChatMessage.java
+++ app/src/main/java/com/example/felipe/androidchat/entities/ChatMessage.java
@@ -1,4 +1,38 @@
 package com.example.felipe.androidchat.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"sentByMe"})
 public class ChatMessage {
+    private String msg;
+    private String sender;
+    private Boolean sentByMe;
+
+    public ChatMessage() {
+
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public Boolean getSentByMe() {
+        return sentByMe;
+    }
+
+    public void setSentByMe(Boolean sentByMe) {
+        this.sentByMe = sentByMe;
+    }
 }
