3bf7f296db23a2427a6454eed53d5cda00927f94
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-27 19:26:44 -0400

    Implemented chatAdapter

--- app/src/main/java/com/example/felipe/androidchat/chat/ui/adapters/ChatAdapter.java
+++ app/src/main/java/com/example/felipe/androidchat/chat/ui/adapters/ChatAdapter.java
@@ -1,37 +1,101 @@
 package com.example.felipe.androidchat.chat.ui.adapters;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.example.felipe.androidchat.R;
 import com.example.felipe.androidchat.entities.ChatMessage;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder>{
     private Context context;
+    private List<ChatMessage> chatMessages;
+
+    public ChatAdapter(Context context,
+                       List<ChatMessage> chatMessages) {
+        this.context = context;
+        this.chatMessages = chatMessages;
+    }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ChatAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.content_chat, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        ChatMessage chatMessage = chatMessages.get(position);
 
+        String msg = chatMessage.getMsg();
+        holder.txtMessage.setText(msg);
+
+        int color = fetchColor(R.attr.colorPrimary);
+        int gravity = Gravity.LEFT;
+
+        if (!chatMessage.getSentByMe()) {
+            gravity = Gravity.RIGHT;
+            color = fetchColor(R.attr.colorAccent);
+        }
+
+        holder.txtMessage.setBackgroundColor(color);
+        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)holder.txtMessage.getLayoutParams();
+        params.gravity = gravity;
+        holder.txtMessage.setLayoutParams(params);
+    }
+
+    private int fetchColor(int color) {
+        TypedValue typedValue = new TypedValue();
+        TypedArray a = context.obtainStyledAttributes(typedValue.data,
+                new int[] { color });
+        int returnColor = a.getColor(0, 0);
+        a.recycle();
+        return returnColor;
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return chatMessages.size();
     }
 
     public void add(ChatMessage msg) {
-
+        if (!alreadyInAdapter(msg)) {
+            this.chatMessages.add(msg);
+            this.notifyDataSetChanged();
+        }
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
-            super(itemView);
+    private boolean alreadyInAdapter(ChatMessage newMsg){
+        boolean alreadyInAdapter = false;
+        for (ChatMessage msg : this.chatMessages) {
+            if (msg.equals(newMsg)) {
+                alreadyInAdapter = true;
+                break;
+            }
+        }
+
+        return alreadyInAdapter;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.txtMessage)
+        TextView txtMessage;
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
         }
     }
 }
