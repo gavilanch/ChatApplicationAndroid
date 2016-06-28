3527d71d0add6e9b904fcb8769856d367d34c94f
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-28 18:59:54 -0400

    Implemented ChatInteractorImpl and ChatSessionInteractorImpl

--- app/src/main/java/com/example/felipe/androidchat/chat/ChatInteractorImpl.java
+++ app/src/main/java/com/example/felipe/androidchat/chat/ChatInteractorImpl.java
@@ -3,28 +3,35 @@ package com.example.felipe.androidchat.chat;
 import com.example.felipe.androidchat.entities.ChatMessage;
 
 public class ChatInteractorImpl implements ChatInteractor {
+
+    ChatRepository chatRepository;
+
+    public ChatInteractorImpl() {
+        this.chatRepository = new ChatRepositoryImpl();
+    }
+
     @Override
     public void setChatRecipient(String recipient) {
-
+        chatRepository.setChatRecipient(recipient);
     }
 
     @Override
     public void sendMessage(ChatMessage msg) {
-
+        chatRepository.sendMessage(msg);
     }
 
     @Override
     public void subscribe() {
-
+        chatRepository.subscribe();
     }
 
     @Override
     public void unsubscribe() {
-
+        chatRepository.unsubscribe();
     }
 
     @Override
     public void destroyListener() {
-
+        chatRepository.destroyListener();
     }
 }
--- app/src/main/java/com/example/felipe/androidchat/chat/ChatSessionInteractorImpl.java
+++ app/src/main/java/com/example/felipe/androidchat/chat/ChatSessionInteractorImpl.java
@@ -1,8 +1,14 @@
 package com.example.felipe.androidchat.chat;
 
 public class ChatSessionInteractorImpl implements ChatSessionInteractor {
+    ChatRepository chatRepository;
+
+    public ChatSessionInteractorImpl() {
+        this.chatRepository = new ChatRepositoryImpl();
+    }
+
     @Override
     public void changeConnectionStatus(boolean online) {
-
+        chatRepository.changeConnectionStatus(online);
     }
 }
