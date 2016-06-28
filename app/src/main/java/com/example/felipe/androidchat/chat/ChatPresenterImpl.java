4f6296bf306e86ae7f9a83c89905059f90dd68e6
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-28 18:53:30 -0400

    Implemented ChatPresenter. Added message property to ChatEvent

--- app/src/main/java/com/example/felipe/androidchat/chat/ChatPresenterImpl.java
+++ app/src/main/java/com/example/felipe/androidchat/chat/ChatPresenterImpl.java
@@ -3,46 +3,64 @@ package com.example.felipe.androidchat.chat;
 import com.example.felipe.androidchat.chat.events.ChatEvent;
 import com.example.felipe.androidchat.chat.ui.ChatView;
 import com.example.felipe.androidchat.entities.ChatMessage;
+import com.example.felipe.androidchat.entities.User;
+import com.example.felipe.androidchat.lib.EventBus;
+import com.example.felipe.androidchat.lib.GreenRobotEventBus;
 
-/**
- * Created by Felipe on 23-Jun-16.
- */
 public class ChatPresenterImpl implements ChatPresenter {
+
+    EventBus eventBus;
+    ChatView chatView;
+    ChatInteractor chatInteractor;
+    ChatSessionInteractor chatSessionInteractor;
+
     public ChatPresenterImpl(ChatView chatView) {
+        this.chatView = chatView;
+        this.eventBus = GreenRobotEventBus.getInstance();
+
+        this.chatInteractor = new ChatInteractorImpl();
+        this.chatSessionInteractor = new ChatSessionInteractorImpl();
     }
 
     @Override
     public void onPause() {
-
+        chatInteractor.unsubscribe();
+        chatSessionInteractor.changeConnectionStatus(User.OFFLINE);
     }
 
     @Override
     public void onResume() {
-
+        chatInteractor.subscribe();
+        chatSessionInteractor.changeConnectionStatus(User.ONLINE);
     }
 
     @Override
     public void onCreate() {
-
+        eventBus.register(this);
     }
 
     @Override
     public void onDestroy() {
-
+        eventBus.unregister(this);
+        chatInteractor.destroyListener();
+        chatView = null;
     }
 
     @Override
     public void setChatRecipient(String recipient) {
-
+        chatInteractor.setChatRecipient(recipient);
     }
 
     @Override
     public void sendMessage(ChatMessage msg) {
-
+        chatInteractor.sendMessage(msg);
     }
 
     @Override
     public void onEventMainThread(ChatEvent event) {
-
+        if (chatView != null) {
+            ChatMessage msg = event.getMessage();
+            chatView.onMessageReceived(msg);
+        }
     }
 }
--- app/src/main/java/com/example/felipe/androidchat/chat/events/ChatEvent.java
+++ app/src/main/java/com/example/felipe/androidchat/chat/events/ChatEvent.java
@@ -1,4 +1,15 @@
 package com.example.felipe.androidchat.chat.events;
 
+import com.example.felipe.androidchat.entities.ChatMessage;
+
 public class ChatEvent {
+     ChatMessage message;
+
+    public ChatMessage getMessage() {
+        return message;
+    }
+
+    public void setMessage(ChatMessage message) {
+        this.message = message;
+    }
 }
