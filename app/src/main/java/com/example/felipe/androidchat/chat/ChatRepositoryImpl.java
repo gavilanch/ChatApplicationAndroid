16366f45806cc3e9e513dcf7b870db17564a8bfa
Felipe Gavilan <elmejor_887@hotmail.com>
2016-06-28 18:55:21 -0400

    Created ChatRepositoryImpl

---
+++ app/src/main/java/com/example/felipe/androidchat/chat/ChatRepositoryImpl.java
@@ -0,0 +1,35 @@
+package com.example.felipe.androidchat.chat;
+
+import com.example.felipe.androidchat.entities.ChatMessage;
+
+public class ChatRepositoryImpl implements ChatRepository {
+    @Override
+    public void setChatRecipient(String recipient) {
+
+    }
+
+    @Override
+    public void sendMessage(ChatMessage msg) {
+
+    }
+
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void unsubscribe() {
+
+    }
+
+    @Override
+    public void destroyListener() {
+
+    }
+
+    @Override
+    public void changeConnectionStatus(boolean online) {
+
+    }
+}
